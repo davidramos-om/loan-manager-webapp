24569e1627dac84dbc47eb5ecaa2abc1156a46d1
David Ramos <davidramos015@gmail.com>
2024-02-24 17:46:11 -0600

    pagina de inicio

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { CallToActionWithAnnotation } from "src/components/WelcomeCardCTA";
+
+const HomePage = () => {
+    return (
+        <CallToActionWithAnnotation />
+    );
+}
+
+export default HomePage;
