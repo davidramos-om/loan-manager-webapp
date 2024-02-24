25956a552151686bb76e90411c878e3e601f0c22
David Ramos <davidramos015@gmail.com>
2024-02-24 17:45:18 -0600

    pagina 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Image, Text } from '@chakra-ui/react';
+
+
+const NotFoundPage: React.FC = () => {
+    return (
+        <Box textAlign="center">
+            <Image src="/path/to/404-image.png" alt="404 Error" />
+            <Text fontSize="xl" fontWeight="bold" mt={4}>
+                Error 404: Pagína no encontrada
+            </Text>
+            <Text mt={2}>
+                Lo sentimos, la página que buscas no existe.
+            </Text>
+        </Box>
+    );
+};
+
+export default NotFoundPage;
