1d4c0c60c8c0a7ba50ab1fdc994ec77112a8fa81
David Ramos <davidramos015@gmail.com>
2024-02-25 02:40:50 -0600

    crear layout para las paginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { lazy } from 'react';
 import { useRoutes } from 'react-router-dom';
 
-const Page404 = lazy(() => import('../pages/404'));
+const DashboardLayout = lazy(() => import('src/components/layout'));
 const HomePage = lazy(() => import('../pages/home'));
+const Page404 = lazy(() => import('../pages/404'));
 const CreateAccountPage = lazy(() => import('../pages/create-account'));
 const AddExistingAccountPage = lazy(() => import('../pages/add-account'));
 const DashboardPage = lazy(() => import('../pages/dashboard'));
@@ -10,28 +11,44 @@ const DashboardPage = lazy(() => import('../pages/dashboard'));
 import paths from './path';
 
 export default function Router() {
-
-    return useRoutes([
-        {
-            path: paths.HOME,
-            element: <HomePage />,
-        },
-        {
-            path: paths.NEW_ACCOUNT,
-            element: <CreateAccountPage />,
-        },
-        {
-            path: paths.ADD_ACCOUNT,
-            element: <AddExistingAccountPage />,
-        },
-        {
-            path: paths.DASHBOARD,
-            element: <DashboardPage />,
-        },
-        {
-            path: paths.NOT_FOUND,
-            element: <Page404 />,
-        },
-        { path: '*', element: <Page404 /> },
-    ]);
+  return useRoutes([
+    {
+      path: paths.HOME,
+      element: (
+        <DashboardLayout>
+          {' '}
+          <HomePage />{' '}
+        </DashboardLayout>
+      ),
+    },
+    {
+      path: paths.NEW_ACCOUNT,
+      element: (
+        <DashboardLayout>
+          <CreateAccountPage />
+        </DashboardLayout>
+      ),
+    },
+    {
+      path: paths.ADD_ACCOUNT,
+      element: (
+        <DashboardLayout>
+          <AddExistingAccountPage />
+        </DashboardLayout>
+      ),
+    },
+    {
+      path: paths.DASHBOARD,
+      element: (
+        <DashboardLayout>
+          <DashboardPage />{' '}
+        </DashboardLayout>
+      ),
+    },
+    {
+      path: paths.NOT_FOUND,
+      element: <Page404 />,
+    },
+    { path: '*', element: <Page404 /> },
+  ]);
 }
